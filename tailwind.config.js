43bcc491fb43ef37feccf11ff865de95d91a9bf0
dammybundlez <hunchosage@gmail.com>
2026-01-10 08:46:41 -0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        geist: ['Geist', 'sans-serif'],
+        shantell: ['Shantell Sans', 'cursive'],
+      },
+    },
+  },
+  plugins: [],
+}
+
