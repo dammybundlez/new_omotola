3108d410075fbfc6012be645c324aa40d7732add
dammybundlez <hunchosage@gmail.com>
2026-01-22 01:13:56 -0800

    next push

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html", "./darkmode.html", "./src/**/*.{js,ts,jsx,tsx}",
+    "./index.html", "./darkmode.html", "./films.html", "./legacy.html", "./news.html", "about.html" , "./**/**/.html",  "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       fontFamily: {
         geist: ['Geist', 'sans-serif'],
         shantell: ['Shantell Sans', 'cursive'],
+        athelas: ['"Athelas"', 'serif'],
+      },
+      keyframes: {
+        softBounce: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-8px)' },
+        },
+      },
+      animation: {
+        softBounce: 'softBounce 2s ease-in-out infinite',
+        softBounceDelay: 'softBounce 2s ease-in-out infinite 0.3s',
       },
     },
   },
